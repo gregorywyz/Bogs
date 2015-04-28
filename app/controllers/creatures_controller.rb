41988279d0b7c0b347569fd2fa34923713a1b825
gregorywyz <gregwyszynski@gmail.com>
2015-04-27 23:44:38 -0700

    w07d01

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,40 @@
+class CreaturesController < ApplicationController
+
+  def index
+    @creatures = Creature.all
+  end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    @creature = Creature.create(creature_params)
+    redirect_to @creature
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+  def edit
+    @creature = Creature.find(params[:id])
+  end
+
+  def update
+    @creature = Creature.update(params[:id], creature_params)
+    redirect_to @creature
+  end
+
+  def destroy
+    @creature = Creature.destroy(params[:id])
+    redirect_to @creature
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name,:desc)
+  end
+
+end
