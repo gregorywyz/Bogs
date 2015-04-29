712fe891cee70c2fc8e12619a3eba56c92632a34
gregorywyz <gregwyszynski@gmail.com>
2015-04-29 16:59:21 -0700

    set up routes for tags controller

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,16 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   resources :creatures, :only => [:index, :new, :create, :show, :edit, :update, :destroy]
 
+  resources :tags
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
