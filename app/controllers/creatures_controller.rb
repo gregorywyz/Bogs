e1d53fea9d5f1e66f59338af5e5792a60a075ea4
gregorywyz <gregwyszynski@gmail.com>
2015-04-29 16:19:56 -0700

    adding tag model and using it in edit and new

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -6,10 +6,16 @@ class CreaturesController < ApplicationController
 
   def new
     @creature = Creature.new
+    @tags = Tag.all
   end
 
   def create
     @creature = Creature.create(creature_params)
+
+    tags = params[:creature][:tag_ids]
+    tags.each do |tag_id|
+      @creature.tags << Tag.find(tag_id) unless tag_id.blank?
+    end
     redirect_to @creature
   end
 
@@ -19,10 +25,19 @@ class CreaturesController < ApplicationController
 
   def edit
     @creature = Creature.find(params[:id])
+    @tags = Tag.all
   end
 
   def update
     @creature = Creature.update(params[:id], creature_params)
+    # @creature << Tag.find(creature_params)
+
+    @creature.tags.clear
+    tags = params[:creature][:tag_ids]
+    tags.each do |tag_id|
+      @creature.tags << Tag.find(tag_id) unless tag_id.blank?
+    end
+
     redirect_to @creature
   end
 
