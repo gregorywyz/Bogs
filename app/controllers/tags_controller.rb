90b2db70c052250dfd0618e6df7635e6a4526f7f
gregorywyz <gregwyszynski@gmail.com>
2015-04-29 18:24:14 -0700

    creating tag CRUD. rendering index, show, and new pages. create and destroy working for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,13 +4,35 @@ class TagsController < ApplicationController
     @tags = Tag.all
   end
 
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
   def new
+    @tag = Tag.new
   end
 
   def create
+    @tag = Tag.create(tag_params)
+    redirect_to tags_path
   end
 
   def destroy
+    @tag = Tag.find(params[:id])
+
+    unless @tag.creatures
+      Tag.destroy(params[:id])
+    else
+      flash[:info] = "Cannot delete this tag. This tag has #{@tag.creatures.length} " + "creature".pluralize(@tag.creatures.length)
+    end
+
+    redirect_to tags_path
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 
 end
