b7216d8ee72e23893a443fc20b57c4b5aa982c5d
gregorywyz <gregwyszynski@gmail.com>
2015-04-30 07:58:06 -0700

    allowing delete of tag in use

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -20,11 +20,23 @@ class TagsController < ApplicationController
   def destroy
     @tag = Tag.find(params[:id])
 
+
+    # unless @tag.creatures
+    #   Tag.destroy(params[:id])
+    # else
+    #   flash[:info] = "Cannot delete this tag. This tag has #{@tag.creatures.length} " + "creature".pluralize(@tag.creatures.length)
+    # end
+
     unless @tag.creatures
       Tag.destroy(params[:id])
     else
-      flash[:info] = "Cannot delete this tag. This tag has #{@tag.creatures.length} " + "creature".pluralize(@tag.creatures.length)
+      flash[:info] = "You removed the #{@tag.name} tag from  #{@tag.creatures.length} " + "creature".pluralize(@tag.creatures.length) + "."
+      @tag.creatures do |creature|
+        creature.tags.delete(params[:id])
+      end
+      Tag.destroy(params[:id])
     end
+
 
     redirect_to tags_path
   end
